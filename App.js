2222243da5fdc18c9ea03249d3d6ea6756b8e436
pravin rathod <pravinrathod206@gmail.com>
2026-04-02 10:06:19 +0530

    added parcel bundler

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     { id: 'parent' },
     React.createElement(
         "div",
         { id: 'child' },
-        [
-            React.createElement("h1", { id: 'heading' }, "Namaste React!!"),
-            React.createElement("h2", { id: 'heading2' }, "Namaste React!!")
-        ]))
+        React.createElement("h1", { id: 'heading' }, "Namaste React!!")))
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
