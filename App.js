442cc265ec9e572a10e6255076683bee12cc83df
pravin rathod <pravinrathod206@gmail.com>
2026-04-03 13:11:32 +0530

    added JSX

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: 'parent' },
-    React.createElement(
-        "div",
-        { id: 'child' },
-        React.createElement("h1", { id: 'heading' }, "Namaste React!!")))
+// const nestedHeaderElement = React.createElement('div', { className: 'title' },
+//     [React.createElement('h1', {}, 'This is h1 tag'),
+//     React.createElement('h2', {}, 'This is h2 tag'),
+//     React.createElement('h3', {}, 'This is h3 tag'),]
+// )
+
+// const nestedHeaderElement = <div class="title">
+//     <h1>This is h1 tag</h1>
+//     <h2>This is h2 tag</h2>
+//     <h3>This is h3 tag</h3>
+// </div>
+const TitleComponent = () => {
+    return (
+        < h1 > This is h1 tag</h1 >
+    )
+}
+const Header = () => {
+    return (
+        <div>
+            Logo
+            Searchbar
+            userIcon
+        </div>
+    )
+}
+const NestedHeaderElement = () => {
+    return (
+        <div className="title">
+            <Header />
+            {TitleComponent()}
+            <TitleComponent />
+            <TitleComponent></TitleComponent>
+            <h1>This is h1 tag</h1>
+            <h2>This is h2 tag</h2>
+            <h3>This is h3 tag</h3>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<NestedHeaderElement />); 
